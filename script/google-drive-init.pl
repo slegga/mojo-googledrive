8889f48349c4da33879eee3331e6c9feabb9d9b9
Stein Hammer <steihamm@gmail.com>
2022-09-28 14:31:19 +0200

    Add refresh token script

---
+++ script/google-drive-init.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+###########################################
+# google-drive-init
+# Mike Schilli, 2014 (m@perlmeister.com)
+# got from Net::Google:Drive::Simple
+# Modified by slegga
+###########################################
+
+=head1 SYNOPSIS
+
+On MAC
+
+mojo-googledrive git:(main) ✗ PERL_LWP_SSL_CA_FILE=/opt/homebrew/share/ca-certificates/cacert.pem script/google-drive-init.pl
+
+=cut
+
+use Mojo::Base -strict;
+use 5.26.0;;
+use Data::Dumper;
+# abort earlier if requires deps are missing
+use LWP::Protocol::https;
+use YAML::Syck;
+use OAuth::Cmdline::GoogleDrive;
+use OAuth::Cmdline::Mojo;
+
+my $client_id = q[XXXXXXXX.apps.googleusercontent.com];
+my $client_secret = q[YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY];
+my $gfile = "$ENV{HOME}/.google-drive.yml";
+
+# To refresh from copy of data from another computer.
+if( -e $gfile ) {
+    my $data = LoadFile($gfile);
+    say Dumper $data;
+    $client_id = $data->{client_id};
+    $client_secret = $data->{client_secret};
+    say `cp $gfile /tmp -v`;
+#    die;
+
+}
+my $oauth = OAuth::Cmdline::GoogleDrive->new(
+    client_id     => $client_id,
+    client_secret => $client_secret,
+    login_uri     => "https://accounts.google.com/o/oauth2/auth",
+    token_uri     => "https://accounts.google.com/o/oauth2/token",
+    scope         => "https://www.googleapis.com/auth/drive",
+    access_type   => "offline",
+);
+
+my $app = OAuth::Cmdline::Mojo->new(
+    oauth => $oauth,
+);
+
+$app->start( 'daemon', '-l', $oauth->local_uri );
